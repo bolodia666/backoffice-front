2d59f1f4a308c9a44f87ab60e892d939b5651363
anakzel <outmadness@gmail.com>
2022-06-08 13:20:32 +0300

    host changes

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,47 @@
-const serverUrl = '';
+const host = window.location.protocol + "//" + window.location.host;
 const rtpElement = document.getElementById('rtp');
+const method = 'POST'
+const headers = {
+	'Content-Type': 'application/json'
+}
+
 document.getElementById('saveBtn').onclick = setRTP;
 init();
 
 async function init() {
-	try {
-		const fetched = await fetch(serverUrl);
-		fetched.ok ? showPanel(await fetched.json()) : onError('Connection error');
-	} catch (error) {
-		onError('Connection error');
-	}
+	const response = await send('/game/settings/get')
+	showPanel(response.data.rtp)
 }
 function showPanel(rtp) {
 	rtpElement.value = rtp;
 	document.getElementById('form').style.visibility = 'visible';
 }
 async function setRTP() {
-	const data = {};
-	data.rtp = rtpElement.value;
-	const request = {
-		method: 'POST',
-		body: JSON.stringify(data),
-		headers: {
-			'Content-Type': 'application/json'
-		}
+	const rtp = rtpElement.value
+	if (rtp && rtp != '') {
+		send('/game/settings/set', { rtp })
 	}
+	else {
+		onError('Wrong rtp')
+	}
+}
+
+async function send(action, params) {
+	const body = JSON.stringify(params)
+	const request = { method, headers, body }
+	const response = {}
 	try {
-		const fetched = await fetch(serverUrl, request);
-		if (fetched.status != 200) onError('Wrong RTP');
+		const fetched = await fetch(host + action, request);
+		fetched.ok ? response.data = await parseResp(fetched) : onError('Connection error');
 	} catch (error) {
-		onError('Connection error');
+		onError('Connection error')
 	}
+	return response
+}
+async function parseResp(fetched) {
+	const isJSON = !fetched.headers.get("content-type").indexOf("application/json")
+	const resp = isJSON ? await fetched.json() : await fetched.text()
+	return resp
 }
 function onError(msg) {
 	alert(msg);
